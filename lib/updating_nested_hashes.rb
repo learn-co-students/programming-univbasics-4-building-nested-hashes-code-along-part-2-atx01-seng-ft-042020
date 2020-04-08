7aa4ef2a120dfc39bea8d3d32c07801f1c97eef4
Nandita Venkat <nanditavenkat@gmail.com>
2020-04-08 17:25:59 +0000

    Done.

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -49,6 +49,6 @@ def add_characters
       }
    }
 
-epic_tragedy [:additional_characters] = [{:name "Prince Escalus"},{:name "Apothecary"}]
+epic_tragedy [:additional_characters] = [{name: "Prince Escalus"},{name: "Apothecary"}]
 epic_tragedy
 end
